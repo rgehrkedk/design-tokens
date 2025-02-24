694fceda21aa3fe87660364de827bb40eed60056
Rasmus <60469087+rgehrkedk@users.noreply.github.com>
2025-02-24 14:17:23 +0000

    “eboksi”

--- design-tokens/index.js
+++ design-tokens/index.js
@@ -70,15 +70,15 @@ async function saveFiles(links) {
 /**
  * Returns Style Dictionary config
  *
- * @param {string} mode1
+ * @param {string} eboks
  * @param {string} mode2
  * @returns {json} Style Dictionary config
  */
-function getStyleDictionaryConfig(mode1, mode2) {
-  const buildDir = [mode1, mode2].join("_");
+function getStyleDictionaryConfig(eboks, mode2) {
+  const buildDir = [eboks, mode2].join("_");
 
   return {
-    source: [`json/tokens/${mode1}.json`, `json/primitives/${mode2}.json`],
+    source: [`json/brand/${eboks}.json`, `json/primitives/${mode2}.json`],
     platforms: {
       web: {
         transformGroup: "web",
